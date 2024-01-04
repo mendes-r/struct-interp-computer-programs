3270b0d578470a349cd3b2ecbe6c393f46f74291
Ricardo Mendes <Ricardo.Mendes@criticaltechworks.com>
2024-01-04 17:17:31 +0000

    ADD solutions printer

--- chapter_1/ex_1_3.scm
+++ chapter_1/ex_1_3.scm
@@ -1,13 +1,20 @@
+;; print
+(define (printx expected result)
+  (format
+    #t
+    "~%Expected: %S | Result: %S~%" expected result))
+
 ;; exercise 1.3
-(define (ex a b c) 
+(define (solution-1 a b c) 
   (cond 
     ((and (< a b) (< a c)) (+ a (* b b) (* c c)))
     ((and (< b a) (< b c)) (+ b (* a a) (* c c)))
     ((= 1 1) (+ c (* a a) (* b b)))))
 
 ;; tests
-(ex 1 2 3) ;; shall return 14
-(ex 2 1 3) ;; shall return 14
-(ex 3 2 1) ;; shall return 14
-(ex 3 2 1) ;; shall return 14
-(ex 2 2 2) ;; shall return 10
+(printx "14" (solution-1 1 2 3))
+(solution-1 1 2 3) ;; shall return 14
+(solution-1 2 1 3) ;; shall return 14
+(solution-1 3 2 1) ;; shall return 14
+(solution-1 3 2 1) ;; shall return 14
+(solution-1 2 2 2) ;; shall return 10
